d8cec155ebc27b7e310284cecd918d947f7c0191
Ficusya <128972041+Ficusya@users.noreply.github.com>
2023-12-03 19:50:50 +0300

    Update Bus_station.sql

--- Bus_station.sql
+++ Bus_station.sql
@@ -1,4 +1,4 @@
-DROP SCHEMA IF EXISTS station ;
+DROP SCHEMA IF EXISTS station;
 
 CREATE SCHEMA IF NOT EXISTS station
     AUTHORIZATION kulishkin_in;
@@ -13,7 +13,7 @@ ALTER ROLE kulishkin_in IN DATABASE kulishkin_in_db
 
 drop table if exists bus, marks, models, employees, positions, bus_stop cascade;
 
-CREATE TABLE IF NOT EXISTS station.Bus (
+CREATE TABLE  IF NOT EXISTS station.Bus (
 	ID serial NOT NULL,
 	Model_id int NOT NULL,
 	Year int NOT NULL CHECK (Year > 1980),
@@ -26,16 +26,16 @@ CREATE TABLE IF NOT EXISTS station.Bus (
   OIDS=FALSE
 );
 
-COMMENT ON TABLE Bus IS 'Автобус';
-COMMENT ON COLUMN Bus.ID IS 'Идентификатор автобуса';
-COMMENT ON COLUMN Bus.Model_id IS 'Идентификатор модели автобуса';
-COMMENT ON COLUMN Bus.Year IS 'Год выпуска автобуса';
-COMMENT ON COLUMN Bus.reg_date IS 'Дата постановки на учет автобуса';
-COMMENT ON COLUMN Bus.retire_date IS 'Дата списания автобуса';
-COMMENT ON COLUMN Bus.last_service_date IS 'Дата прохождения последнего ТО автобуса';
-COMMENT ON COLUMN Bus.gosnum IS 'Государственный номер автобуса';
+COMMENT ON TABLE station.Bus IS 'Автобус';
+COMMENT ON COLUMN station.Bus.ID IS 'Идентификатор автобуса';
+COMMENT ON COLUMN station.Bus.Model_id IS 'Идентификатор модели автобуса';
+COMMENT ON COLUMN station.Bus.Year IS 'Год выпуска автобуса';
+COMMENT ON COLUMN station.Bus.reg_date IS 'Дата постановки на учет автобуса';
+COMMENT ON COLUMN station.Bus.retire_date IS 'Дата списания автобуса';
+COMMENT ON COLUMN station.Bus.last_service_date IS 'Дата прохождения последнего ТО автобуса';
+COMMENT ON COLUMN station.Bus.gosnum IS 'Государственный номер автобуса';
 
-INSERT INTO Bus (Model_id, Year, reg_date, retire_date, last_service_date, gosnum) VALUES
+INSERT INTO station.Bus (Model_id, Year, reg_date, retire_date, last_service_date, gosnum) VALUES
 (1, 2010, '2010-01-01', '2020-12-31', '2020-11-30', 'A001AA'),
 (2, 2015, '2015-05-05', '2025-05-04', '2020-10-10', 'B002BB'),
 (3, 2018, '2018-08-08', '2028-08-07', '2020-09-09', 'C003CC'),
@@ -50,11 +50,11 @@ CREATE TABLE IF NOT EXISTS station.Marks (
   OIDS=FALSE
 );
 
-COMMENT ON TABLE Marks IS 'Марка автобуса';
-COMMENT ON COLUMN Marks.ID IS 'Идентификатор марки автобуса';
-COMMENT ON COLUMN Marks.MarkName IS 'Название марки автобуса';
+COMMENT ON TABLE station.Marks IS 'Марка автобуса';
+COMMENT ON COLUMN station.Marks.ID IS 'Идентификатор марки автобуса';
+COMMENT ON COLUMN station.Marks.MarkName IS 'Название марки автобуса';
 
-INSERT INTO Marks (MarkName) VALUES
+INSERT INTO station.Marks (MarkName) VALUES
 ('Volvo'),
 ('Mercedes'),
 ('Iveco'),
@@ -64,7 +64,7 @@ INSERT INTO Marks (MarkName) VALUES
 CREATE TABLE IF NOT EXISTS station.Models (
 	ID serial NOT NULL,
 	Mark_ID int NOT NULL,
-	ModelName serial NOT NULL UNIQUE,
+	ModelName TEXT NOT NULL UNIQUE,
 	FuelStorage int NOT NULL CHECK (FuelStorage > 0),
 	Capacity int NOT NULL CHECK (Capacity > 0),
 	CONSTRAINT Models_pk PRIMARY KEY (ID)
@@ -72,14 +72,14 @@ CREATE TABLE IF NOT EXISTS station.Models (
   OIDS=FALSE
 );
 
-COMMENT ON TABLE Models IS 'Модель автобуса';
-COMMENT ON COLUMN Models.ID IS 'Уникальный идентификатор модели автобуса';
-COMMENT ON COLUMN Models.Mark_ID IS 'Идентификатор марки автобуса';
-COMMENT ON COLUMN Models.ModelName IS 'Название модели автобуса';
-COMMENT ON COLUMN Models.FuelStorage IS 'Объем топливного бака автобуса';
-COMMENT ON COLUMN Models.Capacity IS 'Вместимость автобуса';
+COMMENT ON TABLE station.Models IS 'Модель автобуса';
+COMMENT ON COLUMN station.Models.ID IS 'Уникальный идентификатор модели автобуса';
+COMMENT ON COLUMN station.Models.Mark_ID IS 'Идентификатор марки автобуса';
+COMMENT ON COLUMN station.Models.ModelName IS 'Название модели автобуса';
+COMMENT ON COLUMN station.Models.FuelStorage IS 'Объем топливного бака автобуса';
+COMMENT ON COLUMN station.Models.Capacity IS 'Вместимость автобуса';
 
-INSERT INTO Models (Mark_ID, ModelName, FuelStorage, Capacity) VALUES
+INSERT INTO station.Models (Mark_ID, ModelName, FuelStorage, Capacity) VALUES
 (1, 'Volvo 9700', 500, 50),
 (2, 'Mercedes-Benz Tourismo', 450, 48),
 (3, 'Iveco Magelys', 400, 46),
@@ -100,17 +100,17 @@ CREATE TABLE IF NOT EXISTS station.Employees (
   OIDS=FALSE
 );
 
-COMMENT ON TABLE Employees IS 'Сотрудник';
-COMMENT ON COLUMN Employees.ID IS 'Уникальный идентификатор сотрудника';
-COMMENT ON COLUMN Employees.first_name IS 'Имя сотрудника';
-COMMENT ON COLUMN Employees.second_name IS 'Отчество сотрудника';
-COMMENT ON COLUMN Employees.last_name IS 'Фамилия сотрудника';
-COMMENT ON COLUMN Employees.birthdate IS 'Дата рождения сотрудника';
-COMMENT ON COLUMN Employees.hire_date IS 'Дата найма сотрудника';
-COMMENT ON COLUMN Employees.retire_date IS 'Дата увольнения сотрудника';
-COMMENT ON COLUMN Employees.pos_id IS 'Идентификатор должности сотрудника';
+COMMENT ON TABLE station.Employees IS 'Сотрудник';
+COMMENT ON COLUMN station.Employees.ID IS 'Уникальный идентификатор сотрудника';
+COMMENT ON COLUMN station.Employees.first_name IS 'Имя сотрудника';
+COMMENT ON COLUMN station.Employees.second_name IS 'Отчество сотрудника';
+COMMENT ON COLUMN station.Employees.last_name IS 'Фамилия сотрудника';
+COMMENT ON COLUMN station.Employees.birthdate IS 'Дата рождения сотрудника';
+COMMENT ON COLUMN station.Employees.hire_date IS 'Дата найма сотрудника';
+COMMENT ON COLUMN station.Employees.retire_date IS 'Дата увольнения сотрудника';
+COMMENT ON COLUMN station.Employees.pos_id IS 'Идентификатор должности сотрудника';
 
-INSERT INTO Employees (first_name, second_name, last_name, birthdate, hire_date, retire_date, pos_id) VALUES
+INSERT INTO station.Employees (first_name, second_name, last_name, birthdate, hire_date, retire_date, pos_id) VALUES
 ('Иван', 'Иванович', 'Иванов', '1980-01-01', '2010-01-01', '2030-01-01', 1),
 ('Петр', 'Петрович', 'Петров', '1981-02-02', '2011-02-02', '2031-02-02', 2),
 ('Сергей', 'Сергеевич', 'Сергеев', '1982-03-03', '2012-03-03', '2032-03-03', 3),
@@ -125,11 +125,11 @@ CREATE TABLE IF NOT EXISTS station.Positions (
   OIDS=FALSE
 );
 
-COMMENT ON TABLE Positions IS 'Должность';
-COMMENT ON COLUMN Positions.ID IS 'Уникальный идентификатор должности';
-COMMENT ON COLUMN Positions.pos_name IS 'Название должности';
+COMMENT ON TABLE station.Positions IS 'Должность';
+COMMENT ON COLUMN station.Positions.ID IS 'Уникальный идентификатор должности';
+COMMENT ON COLUMN station.Positions.pos_name IS 'Название должности';
 
-INSERT INTO Positions (pos_name) VALUES
+INSERT INTO station.Positions (pos_name) VALUES
 ('Директор'),
 ('Заместитель директора'),
 ('Бухгалтер'),
@@ -144,11 +144,11 @@ CREATE TABLE IF NOT EXISTS station.bus_stop (
   OIDS=FALSE
 );
 
-COMMENT ON TABLE bus_stop IS 'Остановка';
-COMMENT ON COLUMN bus_stop.ID IS 'Уникальный идентификатор остановки';
-COMMENT ON COLUMN bus_stop.bs_name IS 'Название остановки';
+COMMENT ON TABLE station.bus_stop IS 'Остановка';
+COMMENT ON COLUMN station.bus_stop.ID IS 'Уникальный идентификатор остановки';
+COMMENT ON COLUMN station.bus_stop.bs_name IS 'Название остановки';
 
-CREATE TABLE routes (
+CREATE TABLE station.routes (
 	ID serial NOT NULL,
 	route_name TEXT NOT NULL,
 	start_point_id int NOT NULL,
@@ -157,12 +157,12 @@ CREATE TABLE routes (
   OIDS=FALSE
 );	
 
-COMMENT ON TABLE routes IS 'Маршрут';
-COMMENT ON COLUMN routes.ID IS 'Уникальный идентификатор маршрута';
-COMMENT ON COLUMN routes.route_name IS 'Название маршрута';
-COMMENT ON COLUMN routes.start_point_id IS 'Идентификатор начальной остановки маршрута';
+COMMENT ON TABLE station.routes IS 'Маршрут';
+COMMENT ON COLUMN station.routes.ID IS 'Уникальный идентификатор маршрута';
+COMMENT ON COLUMN station.routes.route_name IS 'Название маршрута';
+COMMENT ON COLUMN station.routes.start_point_id IS 'Идентификатор начальной остановки маршрута';
 
-CREATE TABLE route_points (
+CREATE TABLE station.route_points (
 	ID serial,
 	route_id int NOT NULL,
 	bus_stop_id int NOT NULL,
@@ -172,17 +172,17 @@ CREATE TABLE route_points (
   OIDS=FALSE
 );
 
-COMMENT ON TABLE route_points IS 'Точка маршрута';
-COMMENT ON COLUMN route_points.ID IS 'Уникальный идентификатор точки маршрута';
-COMMENT ON COLUMN route_points.route_id IS 'Идентификатор маршрута';
-COMMENT ON COLUMN route_points.bus_stop_id IS 'Идентификатор остановки';
-COMMENT ON COLUMN route_points.next_point_id IS 'Идентификатор следующей точки маршрута';
+COMMENT ON TABLE station.route_points IS 'Точка маршрута';
+COMMENT ON COLUMN station.route_points.ID IS 'Уникальный идентификатор точки маршрута';
+COMMENT ON COLUMN station.route_points.route_id IS 'Идентификатор маршрута';
+COMMENT ON COLUMN station.route_points.bus_stop_id IS 'Идентификатор остановки';
+COMMENT ON COLUMN station.route_points.next_point_id IS 'Идентификатор следующей точки маршрута';
 
-CREATE TABLE Race (
+CREATE TABLE station.Race (
 	ID serial NOT NULL,
-	start_date serial NOT NULL,
-	finish_date serial NOT NULL,
-	duration serial NOT NULL,
+	start_date TEXT NOT NULL,
+	finish_date TEXT NOT NULL,
+	duration TEXT NOT NULL,
 	route_id int NOT NULL,
 	bus_id int NOT NULL,
 	driver_id int NOT NULL,
@@ -191,16 +191,16 @@ CREATE TABLE Race (
   OIDS=FALSE
 );
 
-COMMENT ON TABLE Race IS 'Рейс';
-COMMENT ON COLUMN Race.ID IS 'Уникальный идентификатор рейса';
-COMMENT ON COLUMN Race.start_date IS 'Дата и время начала рейса';
-COMMENT ON COLUMN Race.finish_date IS 'Дата и время окончания рейса';
-COMMENT ON COLUMN Race.duration IS 'Продолжительность рейса';
-COMMENT ON COLUMN Race.route_id IS 'Идентификатор маршрута';
-COMMENT ON COLUMN Race.bus_id IS 'Идентификатор автобуса';
-COMMENT ON COLUMN Race.driver_id IS 'Идентификатор водителя';
+COMMENT ON TABLE station.Race IS 'Рейс';
+COMMENT ON COLUMN station.Race.ID IS 'Уникальный идентификатор рейса';
+COMMENT ON COLUMN station.Race.start_date IS 'Дата и время начала рейса';
+COMMENT ON COLUMN station.Race.finish_date IS 'Дата и время окончания рейса';
+COMMENT ON COLUMN station.Race.duration IS 'Продолжительность рейса';
+COMMENT ON COLUMN station.Race.route_id IS 'Идентификатор маршрута';
+COMMENT ON COLUMN station.Race.bus_id IS 'Идентификатор автобуса';
+COMMENT ON COLUMN station.Race.driver_id IS 'Идентификатор водителя';
 
-CREATE TABLE Passangers (
+CREATE TABLE station.Passangers (
 	ID serial NOT NULL,
 	first_name TEXT NOT NULL,
 	second_name TEXT NOT NULL,
@@ -210,25 +210,25 @@ CREATE TABLE Passangers (
 	doc_series int NOT NULL,
 	doc_num int NOT NULL,
 	sex TEXT NOT NULL,
-	phone_num int NOT NULL,
+	phone_num TEXT NOT NULL,
 	CONSTRAINT Passangers_pk PRIMARY KEY (ID)
 ) WITH (
   OIDS=FALSE
 );
 
-COMMENT ON TABLE Passangers IS 'Пассажиры';
-COMMENT ON COLUMN Passangers.ID IS 'Уникальный идентификатор пассажира';
-COMMENT ON COLUMN Passangers.first_name IS 'Имя пассажира';
-COMMENT ON COLUMN Passangers.second_name IS 'Отчество пассажира';
-COMMENT ON COLUMN Passangers.last_name IS 'Фамилия пассажира';
-COMMENT ON COLUMN Passangers.birthdate IS 'Дата рождения пассажира';
-COMMENT ON COLUMN Passangers.doc_type IS 'Тип документа пассажира';
-COMMENT ON COLUMN Passangers.doc_series IS 'Серия документа пассажира';
-COMMENT ON COLUMN Passangers.doc_num IS 'Номер документа пассажира';
-COMMENT ON COLUMN Passangers.sex IS 'Пол пассажира';
-COMMENT ON COLUMN Passangers.phone_num IS 'Номер телефона пассажира';
+COMMENT ON TABLE station.Passangers IS 'Пассажиры';
+COMMENT ON COLUMN station.Passangers.ID IS 'Уникальный идентификатор пассажира';
+COMMENT ON COLUMN station.Passangers.first_name IS 'Имя пассажира';
+COMMENT ON COLUMN station.Passangers.second_name IS 'Отчество пассажира';
+COMMENT ON COLUMN station.Passangers.last_name IS 'Фамилия пассажира';
+COMMENT ON COLUMN station.Passangers.birthdate IS 'Дата рождения пассажира';
+COMMENT ON COLUMN station.Passangers.doc_type IS 'Тип документа пассажира';
+COMMENT ON COLUMN station.Passangers.doc_series IS 'Серия документа пассажира';
+COMMENT ON COLUMN station.Passangers.doc_num IS 'Номер документа пассажира';
+COMMENT ON COLUMN station.Passangers.sex IS 'Пол пассажира';
+COMMENT ON COLUMN station.Passangers.phone_num IS 'Номер телефона пассажира';
 
-CREATE TABLE RaceList (
+CREATE TABLE station.RaceList (
 	ID serial NOT NULL,
 	race_id int NOT NULL,
 	passanger_id int NOT NULL,
@@ -240,15 +240,15 @@ CREATE TABLE RaceList (
   OIDS=FALSE
 );
 
-COMMENT ON TABLE RaceList IS 'Список рейсов';
-COMMENT ON COLUMN RaceList.ID IS 'Уникальный идентификатор записи в списке рейсов';
-COMMENT ON COLUMN RaceList.race_id IS 'Идентификатор рейса';
-COMMENT ON COLUMN RaceList.passanger_id IS 'Идентификатор пассажира';
-COMMENT ON COLUMN RaceList.seat_num IS 'Номер места';
-COMMENT ON COLUMN RaceList.route_start IS 'Идентификатор начальной точки маршрута';
-COMMENT ON COLUMN RaceList.route_finish IS 'Идентификатор конечной точки маршрута';
+COMMENT ON TABLE station.RaceList IS 'Список рейсов';
+COMMENT ON COLUMN station.RaceList.ID IS 'Уникальный идентификатор записи в списке рейсов';
+COMMENT ON COLUMN station.RaceList.race_id IS 'Идентификатор рейса';
+COMMENT ON COLUMN station.RaceList.passanger_id IS 'Идентификатор пассажира';
+COMMENT ON COLUMN station.RaceList.seat_num IS 'Номер места';
+COMMENT ON COLUMN station.RaceList.route_start IS 'Идентификатор начальной точки маршрута';
+COMMENT ON COLUMN station.RaceList.route_finish IS 'Идентификатор конечной точки маршрута';
 
-INSERT INTO bus_stop (bs_name) VALUES
+INSERT INTO station.bus_stop (bs_name) VALUES
 ('Автовокзал'),
 ('Площадь Ленина'),
 ('Улица Гагарина'),
@@ -256,13 +256,13 @@ INSERT INTO bus_stop (bs_name) VALUES
 ('ТЦ "Мега"'),
 ('Аэропорт');
 
-INSERT INTO routes (route_name, start_point_id) VALUES
+INSERT INTO station.routes (route_name, start_point_id) VALUES
 ('Автовокзал - Аэропорт', 1),
 ('Аэропорт - Автовокзал', 6),
 ('Площадь Ленина - ТЦ "Мега"', 2),
 ('ТЦ "Мега" - Площадь Ленина', 5);
 
-INSERT INTO route_points (route_id, bus_stop_id, next_point_id) VALUES
+INSERT INTO station.route_points (route_id, bus_stop_id, next_point_id) VALUES
 (1, 1, 2),
 (1, 2, 3),
 (1, 3, 4),
@@ -280,7 +280,7 @@ INSERT INTO route_points (route_id, bus_stop_id, next_point_id) VALUES
 (4, 4, 3),
 (4, 3, 2);
 
-INSERT INTO Race (start_date, finish_date, duration, route_id, bus_id, driver_id) VALUES
+INSERT INTO station.Race (start_date, finish_date, duration, route_id, bus_id, driver_id) VALUES
 ('2023-12-01 08:00:00', '2023-12-01 09:30:00', '01:30:00', 1, 1, 1),
 ('2023-12-01 10:00:00', '2023-12-01 11:30:00', '01:30:00', 2, 1, 1),
 ('2023-12-01 12:00:00', '2023-12-01 13:00:00', '01:00:00', 3, 2, 2),
@@ -290,16 +290,16 @@ INSERT INTO Race (start_date, finish_date, duration, route_id, bus_id, driver_id
 ('2023-12-01 20:00:00', '2023-12-01 21:00:00', '01:00:00', 3, 4, 4),
 ('2023-12-01 22:00:00', '2023-12-01 23:00:00', '01:00:00', 4, 4, 4);
 
-INSERT INTO Passangers (first_name, second_name, last_name, birthdate, doc_type, doc_series, doc_num, sex, phone_num) VALUES
-('Иван', 'Иванович', 'Иванов', '1990-01-01', 1, 1111, 111111, 'М', 1111111111),
-('Петр', 'Петрович', 'Петров', '1991-02-02', 1, 2222, 222222, 'М', 2222222222),
-('Анна', 'Андреевна', 'Андреева', '1992-03-03', 1, 3333, 333333, 'Ж', 3333333333),
-('Мария', 'Михайловна', 'Михайлова', '1993-04-04', 1, 4444, 444444, 'Ж', 4444444444),
-('Сергей', 'Сергеевич', 'Сергеев', '1994-05-05', 1, 5555, 555555, 'М', 5555555555),
-('Елена', 'Евгеньевна', 'Евгеньева', '1995-06-06', 1, 6666, 666666, 'Ж', 6666666666),
-('Дмитрий', 'Дмитриевич', 'Дмитриев', '1996-07-07', 1, 7777, 777777, 'М', 7777777777);
+INSERT INTO station.Passangers (first_name, second_name, last_name, birthdate, doc_type, doc_series, doc_num, sex, phone_num) VALUES
+('Иван', 'Иванович', 'Иванов', '1990-01-01', 1, 1111, 111111, 'М', '1111111111'),
+('Петр', 'Петрович', 'Петров', '1991-02-02', 1, 2222, 222222, 'М', '2222222222'),
+('Анна', 'Андреевна', 'Андреева', '1992-03-03', 1, 3333, 333333, 'Ж', '3333333333'),
+('Мария', 'Михайловна', 'Михайлова', '1993-04-04', 1, 4444, 444444, 'Ж', '4444444444'),
+('Сергей', 'Сергеевич', 'Сергеев', '1994-05-05', 1, 5555, 555555, 'М', '5555555555'),
+('Елена', 'Евгеньевна', 'Евгеньева', '1995-06-06', 1, 6666, 666666, 'Ж', '6666666666'),
+('Дмитрий', 'Дмитриевич', 'Дмитриев', '1996-07-07', 1, 7777, 777777, 'М', '7777777777');
 
-ALTER TABLE Bus ADD CONSTRAINT Bus_fk0 FOREIGN KEY (Model_id) REFERENCES Models(ID) ON UPDATE CASCADE ON DELETE RESTRICT;
+ALTER TABLE Bus ADD CONSTRAINT Bus_fk_Model_id FOREIGN KEY (Model_id) REFERENCES Models(ID) ON UPDATE CASCADE ON DELETE RESTRICT;
 
 ALTER TABLE Models ADD CONSTRAINT Models_fk_Mark_ID FOREIGN KEY (Mark_ID) REFERENCES Marks(ID) ON UPDATE CASCADE ON DELETE RESTRICT;
 
